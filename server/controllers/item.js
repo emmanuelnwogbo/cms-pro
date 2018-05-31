340b33fad4b47e8991e72f8e2544d9150d52419c
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-05-31 15:00:08 +0100

    [Feature add-item]Added item model to controller route

--- server/controllers/item.js
+++ server/controllers/item.js
@@ -1,5 +1,11 @@
+import Models from '../models';
+
+const {
+  Item
+} = Models;
+
 export default class ItemControllers {
   static postitem(req, res) {
-    console.log('posting item')
+    console.log('posting item');
   }
 }
